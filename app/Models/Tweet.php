6fff916940857c7b86a0f8b09a3f6120c8df9d19
Amine Akacha <akachamohamedamine@gmail.com>
2021-02-12 21:47:18 +0100

    Create followings table

--- app/Models/Tweet.php
+++ app/Models/Tweet.php
@@ -6,6 +6,7 @@ use Carbon\Carbon;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tweet extends Model
 {
@@ -21,5 +22,25 @@ class Tweet extends Model
     public function getCreatedAtAttribute($date)
     {
         return Carbon::parse($date)->format('d M. Y');
+    }
+
+    /**
+     * The followings that belong to the Tweet
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function followings(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'followings', 'follower_id', 'following_id');
+    }
+
+    /**
+     * The followers that belong to the Tweet
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function followers(): BelongsToMany
+    {
+        return $this->belongsToMany(User::class, 'followings', 'following_id', 'follower_id');
     }
 }
